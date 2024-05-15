f51353d69bf0d74a1e47068a6c69bbfacb273242
Diego Borba <diegoborba25.contato@gmail.com>
2024-05-14 21:36:00 -0300

    feat: spring boot added

---
+++ src/main/java/com/diegoborbadev/expensemanager/ExpenseManagerApplication.java
@@ -0,0 +1,11 @@
+package com.diegoborbadev.expensemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagerApplication.class, args);
+	}
+}
