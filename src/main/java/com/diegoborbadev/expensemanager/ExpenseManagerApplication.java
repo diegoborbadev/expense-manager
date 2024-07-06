31aed76bc7eb6181cd2f4a3ea7a30d3217dc5f8b
Diego Borba <diegoborba25.contato@gmail.com>
2024-07-06 00:33:39 -0300

    fix: formatation

--- src/main/java/com/diegoborbadev/expensemanager/ExpenseManagerApplication.java
+++ src/main/java/com/diegoborbadev/expensemanager/ExpenseManagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExpenseManagerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ExpenseManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagerApplication.class, args);
+    }
 }
